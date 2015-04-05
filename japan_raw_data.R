cff7f7c585af412a7228e1f96e336c6afc42e6a8
Jonathan Ma <jonbma@gmail.com>
2015-04-04 21:11:29 -0400

    fixing up glm but broke :(

--- japan_raw_data.R
+++ japan_raw_data.R
@@ -46,8 +46,17 @@ plot(zoo.JP$JAPRECD, col = "red", axes = FALSE, ann = FALSE)
 par(new=TRUE)
 plot(zoo.JP$CONCONF, col = "blue", axes = TRUE)
 
-plot(zoo.JP_lag0$JAPRECD, col = "red", axes = FALSE, ann = FALSE)
+plot(zoo.JP$JAPRECD, col = "red", axes = FALSE, ann = FALSE)
 par(new=TRUE)
-plot(zoo.JP$JPNVT0060, col = "blue", axes = TRUE)
+plot(zoo.JP$JPNVT0060,col = "blue", axes = TRUE, xlab = "Year", ylab = "Import Raw Material in Yen", main = "Japan Raw Data 1978-2014: Raw Material and Recession" )
+
+
 plot(zoo.JP_lag0$JPNVT0060, col = "blue", axes = TRUE)
 
+raw_pre1992 = window(zoo.JP$JPNVT0060, start = "1963-01-01", end = "1991-12-01")
+raw_pre1992_demean = raw_pre1992 - mean(raw_pre1992)
+raw_post1992 = window(zoo.JP$JPNVT0060, start = "1992-1-01")
+raw_post1992_demean = raw_post1992 - mean(raw_post1992)
+plot(raw_pre1992_demean)
+
+plot(rbind(raw_pre1992_demean, raw_post1992_demean))
